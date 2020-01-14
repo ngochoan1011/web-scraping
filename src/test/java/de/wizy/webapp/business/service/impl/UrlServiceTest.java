7c4a352d7b9e0f530b9074e98b8a37dfbb61f3ac
HoanDo <hoan.yesi1@gmail.com>
2020-01-14 16:46:02 +0700

    Increase unit test coverage

--- src/test/java/de/wizy/webapp/business/service/impl/UrlServiceTest.java
+++ src/test/java/de/wizy/webapp/business/service/impl/UrlServiceTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import de.wizy.webapp.business.dto.HtmlParserAnalysisDTO;
 import de.wizy.webapp.business.dto.StatusDTO;
 import de.wizy.webapp.business.enums.ErrorCodes;
 import de.wizy.webapp.business.service.UrlService;
@@ -24,6 +25,9 @@ public class UrlServiceTest {
 	private UrlService urlService;
 
 	private final String unreachableUrl = "http://www.qwertytesturl.com";
+	private final String analysisUrl = "https://www.w3schools.com/";
+	private final String urlWithLoginForm = "https://www.facebook.com/";
+	private final String urlNoLoginForm = "https://vnexpress.net/";
 	private final String blankUrl = "";
 
 	@Test
@@ -72,6 +76,37 @@ public class UrlServiceTest {
 		} catch (BadRequestException e) {
 			StatusDTO jsoupParsingError = new StatusDTO(ErrorCodes.JSOUP_PARSING_ERROR);
 			assertEquals(e.getStatusDTO().getCode(), jsoupParsingError.getCode());
+		}
+	}
+
+	@Test
+	public void shouldCheckDocumentTitleAndHTMLVersion() {
+		try {
+			HtmlParserAnalysisDTO analysisResult = urlService.analyse(analysisUrl);
+			assertEquals(analysisResult.getPageTitle(), "W3Schools Online Web Tutorials");
+			assertEquals(analysisResult.getHtmlVersion(), "HTML 5");
+		} catch (BadRequestException e) {
+			throw e;
+		}
+	}
+
+	@Test
+	public void showReturnIsLoginFormIsTrueWithTheURLContainLoginForm() {
+		try {
+			HtmlParserAnalysisDTO analysisResult = urlService.analyse(urlWithLoginForm);
+			assertEquals(analysisResult.getLoginForm(), true);
+		} catch (BadRequestException e) {
+			throw e;
+		}
+	}
+
+	@Test
+	public void showReturnIsLoginFormIsFalseWithTheURLNotContainLoginForm() {
+		try {
+			HtmlParserAnalysisDTO analysisResult = urlService.analyse(urlNoLoginForm);
+			assertEquals(analysisResult.getLoginForm(), false);
+		} catch (BadRequestException e) {
+			throw e;
 		}
 	}
 }
